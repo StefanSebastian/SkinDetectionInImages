b719dd5110bf5d9214427b2e8adee0da16b4cf01
StefanSebastian <stefansebii@gmail.com>
2018-01-08 00:47:10 +0200

    Fixed some bugs

--- licenta/quickshift/quickshift.py
+++ licenta/quickshift/quickshift.py
@@ -3,7 +3,7 @@ import numpy as np
 from math import sqrt
 
 # read initial image
-img = cv2.imread('rsz_cat.jpg')
+img = cv2.imread('cat.jpg')
 print('Shape of image' + str(img.shape))
 print('Shape of pixel' + str(img[0][0].shape))
 
@@ -13,7 +13,7 @@ def point_distance(x1, y1, x2, y2):
     x2 = int(x2)
     y1 = int(y1)
     y2 = int(y2)
-    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
 
 
 def point_distance_3d(x1, y1, z1, x2, y2, z2):
@@ -23,7 +23,7 @@ def point_distance_3d(x1, y1, z1, x2, y2, z2):
     y2 = int(y2)
     z1 = int(z1)
     z2 = int(z2)
-    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1)**2)
+    return sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2) + ((z2 - z1)**2))
 
 
 def compute_density(image, sigma):
@@ -37,6 +37,8 @@ def compute_density(image, sigma):
             # for each pixel
             pixel = image[x_pixel, y_pixel]
 
+            densities[x_pixel, y_pixel] = 0
+
             progress = x_pixel * cols + y_pixel
             if progress % 300 == 0:
                 progress = progress / (rows * cols)
@@ -44,12 +46,12 @@ def compute_density(image, sigma):
                 print(progress, "%")
 
             # get upper corner of window
-            x_upper = x_pixel - 3 * sigma
-            y_upper = y_pixel - 3 * sigma
+            x_upper = x_pixel - 5 * sigma
+            y_upper = y_pixel - 5 * sigma
 
             # get lower corner of window
-            x_lower = x_pixel + 3 * sigma
-            y_lower = y_pixel + 3 * sigma
+            x_lower = x_pixel + 5 * sigma
+            y_lower = y_pixel + 5 * sigma
 
             # iterate neighbourhood
             for x_candidate in range(x_upper, x_lower + 1):
@@ -60,7 +62,7 @@ def compute_density(image, sigma):
                             point = image[x_candidate, y_candidate]
                             # distance between pixel values ; f[x] - f[n]
                             distance = point_distance_3d(point[0], point[1], point[2], pixel[0], pixel[1], pixel[2])
-                            densities[x_pixel, y_pixel] += np.exp(-distance ** 2 / 2 * sigma ** 2)
+                            densities[x_pixel, y_pixel] += np.exp((-(distance ** 2)) / (2 * sigma * sigma))
     return densities
 
 
@@ -105,6 +107,8 @@ densitiesC = compute_density(img, 4)
 print("Get parents")
 parentsC = link_neighbours(img, 10, densitiesC)
 
+img2 = np.zeros(img.shape)
+
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
         parent = parentsC[i, j]
@@ -113,9 +117,9 @@ for i in range(img.shape[0]):
         y_parent = int(parent[1])
         parentPixel = img[x_parent, y_parent]
         print("parent: " + str(parentPixel))
-        img[i, j] = parentPixel
+        img2[i, j] = parentPixel
 
-cv2.imwrite('quickshift.png', img)
+cv2.imwrite('quickshift.png', img2)
 
 
 '''

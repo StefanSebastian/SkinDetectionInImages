8e5b0cead1ae5b96cb3c9af8b4c3315526fd5675
StefanSebastian <stefansebii@gmail.com>
2018-01-11 20:00:55 +0200

    Some package refactor

---
+++ licenta/contours_fill_project/contours.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+def get_image_contours(image):
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    cv2.imwrite("thresh.png", thresh)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(im2, contours, -1, (0, 255, 0), 3)
+    __get_contour_stats(contours)
+    return contours
+
+
+def __get_contour_stats(contours):
+    print("Found : ", len(contours), " contours")
+    one_point_contours = 0
+    for i in range(len(contours)):
+        contour = contours[i]
+        if contour.shape[0] <= 2:
+            one_point_contours += 1
+    print(one_point_contours, " have 1 or 2 points")
--- licenta/quickshift_project/main.py
+++ licenta/quickshift_project/main.py
@@ -1,8 +1,8 @@
 import cv2
-from quickshift_project.algorithm.quickshift import quickshift_algorithm
 
+from quickshift_project.quickshift import quickshift_algorithm
 
-img = cv2.imread('input_data/PASCAL2007/human_cat.jpg')
-res = quickshift_algorithm(img, 3, 10, 1)
-cv2.imwrite("human_cat_3_10.png", res)
+img = cv2.imread('../resources/input_data/pisi.jpg')
+res = quickshift_algorithm(img, 2, 4, 1)
+cv2.imwrite("pisi_2_4.png", res)
 

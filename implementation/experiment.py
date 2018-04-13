9747d5cf8909233e49b8db92bac05ff29e739819
StefanSebastian <stefansebii@gmail.com>
2018-04-13 17:11:17 +0300

    Added another run

--- implementation/experiment.py
+++ implementation/experiment.py
@@ -1,28 +1,20 @@
 import cv2
 
 from utils.utils import load_images_from_folder
-from segmentation.quickshift import QuickshiftSegmentation
-from color_analysis.detect.detector import SpmDetectorFactory
+from texture_analysis.train.trainer import TextureTrainer
+TextureTrainer.train_default_model()
 
-from color_analysis.train.trainer import SPMModelTrainer
-SPMModelTrainer.create_default_spm_trainer()
+image = cv2.imread('E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/PASCAL2007/baby.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+
 
 '''
-qs = QuickshiftSegmentation(1, 3, 5)
-detector = SpmDetectorFactory.get_detector('color_analysis/models/spm_compaq_2000_with_ns_ycbcr.pkl', 2, 2)
-pascalim = load_images_from_folder('E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/PASCAL2007')
-
-
-image_index = 0
-for image in pascalim:
-    image_index += 1
-    image = cv2.resize(image, (200, 200))
-
-    superpixels = qs.get_superpixels(image)
-
-    threshold = 0.1
-    while threshold < 1:
-        threshold += 0.05
-        spmim = detector.detect(image, superpixels, threshold)
-        cv2.imwrite(str(image_index) + '_' + str(threshold) + '.png', spmim)
-'''
+from skimage.feature import greycomatrix, greycoprops
+image = cv2.imread('E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/PASCAL2007/baby.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+glcm = greycomatrix(image, [5], [0], 256, symmetric=True, normed=True)
+features = [greycoprops(glcm, prop='contrast')[0][0], greycoprops(glcm, prop='homogeneity')[0][0],
+                    greycoprops(glcm, prop='energy')[0][0]]
+print(features)
+print(greycoprops(glcm, prop='contrast'))'''

11251bac2e84e045eebe93d8cb1d1667a9c9fb53
StefanSebastian <stefansebii@gmail.com>
2018-04-14 09:02:24 +0300

    Added a new method of calculating color probability

--- implementation/color_analysis/detect/average_on_superpixel_detector.py
+++ implementation/color_analysis/detect/average_on_superpixel_detector.py
@@ -32,7 +32,7 @@ class AverageOnSuperpixelDetector:
                     if p in self.pixel_probability_cache:
                         prob = self.pixel_probability_cache[p]
                     else:
-                        prob = self.get_probability_with_neighbours(image[x_pixel, y_pixel])
+                        prob = self.__calculate_pixel_probability_sum_neighbours(image[x_pixel, y_pixel])
                         self.pixel_probability_cache[p] = prob
                 total_prob += prob
                 count += 1
@@ -45,7 +45,7 @@ class AverageOnSuperpixelDetector:
 
         return new_image
 
-    def get_probability_with_neighbours(self, pixel):
+    def __calculate_pixel_probability_max_neighbours(self, pixel):
         max_prob = 0
         area = self.window_size
         for r_offset in range(-area, area, 1):
@@ -56,3 +56,35 @@ class AverageOnSuperpixelDetector:
                     if prob > max_prob:
                         max_prob = prob
         return max_prob
+
+    def __calculate_pixel_probability_sum_neighbours(self, pixel):  #  todo remove if it doesnt work
+        area = self.window_size
+
+        total_appearances = 0
+        total_appearances_as_skin = 0
+
+        for r_offset in range(-area, area, 1):
+            for g_offset in range(-area, area, 1):
+                for b_offset in range(-area, area, 1):
+
+                    neighbour = [pixel[0] + r_offset, pixel[1] + g_offset, pixel[2] + b_offset]
+                    p = Pixel(F1=neighbour[0], F2=neighbour[1], F3=neighbour[2])
+                    if p not in self.model.components.appearances_as_skin:
+                        appearances_as_skin = 0
+                    else:
+                        appearances_as_skin = self.model.components.appearances_as_skin[p]
+                    total_appearances_as_skin += appearances_as_skin
+                    if p not in self.model.components.appearances:
+                        appearances = 0
+                    else:
+                        appearances = self.model.components.appearances[p]
+                    total_appearances += appearances
+
+        px = total_appearances / (
+            self.model.components.skin_pixels + self.model.components.non_skin_pixels)
+        ps = self.model.components.skin_pixels / (
+            self.model.components.skin_pixels + self.model.components.non_skin_pixels)
+        pxs = total_appearances_as_skin / self.model.components.skin_pixels
+        if px == 0:
+            return 0
+        return (pxs * ps) / px

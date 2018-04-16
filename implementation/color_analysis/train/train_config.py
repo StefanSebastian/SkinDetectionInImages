672f5abfb662f77b2178cdd8cb56c6a16ebfb37e
StefanSebastian <stefansebii@gmail.com>
2018-04-16 20:15:29 +0300

    Mode docs

--- implementation/color_analysis/train/train_config.py
+++ implementation/color_analysis/train/train_config.py
@@ -4,7 +4,7 @@ Default values for training
 path_pos = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/sfa/train/positive'
 path_neg = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/sfa/train/negative'
 path_models = 'E:/Info/anu3/Licenta-git-2/Licenta/implementation/color_analysis/models'
-selected_model = 'spm_compaq_3300_with_ns_rgb.pkl'
-path_compaq = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/compaq-filtered-exp/train'
+selected_model = 'spm_compaq_2000_with_ns_4000_rgb.pkl'
+path_compaq = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/compaq-filtered-fin/train'
 database = 'compaq'  # compaq or sfa
 color_space = 'RGB'
--- implementation/evaluator/run_config.py
+++ implementation/evaluator/run_config.py
@@ -1,10 +1,10 @@
 #  quickshift params
-sigma = 2
-tau = 4
+sigma = 3
+tau = 5
 with_position = 1
 
 #  spm params
-spm_model_path = 'E:/Info/anu3/Licenta-git-2/Licenta/implementation/color_analysis/models/spm_compaq_2000_with_ns_rgb.pkl'
+spm_model_path = 'E:/Info/anu3/Licenta-git-2/Licenta/implementation/color_analysis/models/spm_compaq_2000_with_ns_4000_rgb.pkl'
 threshold = 0.08
 spm_type = 2
 neighbour_area = 4
@@ -15,8 +15,8 @@ detection_type = 1  # 0 - sliding window, 1 - builds windows around each pixel
 window_size = 5
 
 #  testing data
-test_path_in = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/compaq-filtered/validate/input'
-test_path_expected = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/compaq-filtered/validate/mask'
+test_path_in = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/compaq-filtered-fin/validate/input'
+test_path_expected = 'E:/Info/anu3/Licenta-git-2/Licenta/licenta/resources/input_data/compaq-filtered-fin/validate/mask'
 
 #  results path
 results_path = 'E:/Info/anu3/Licenta-git-2/Licenta/implementation/evaluator/run_results'

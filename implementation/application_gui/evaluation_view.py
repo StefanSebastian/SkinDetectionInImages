a7f93568f585a44b03e2d7cec440fa64000e384e
StefanSebastian <stefansebii@gmail.com>
2018-04-21 18:18:49 +0300

    made ui more generic

--- implementation/application_gui/evaluation_view.py
+++ implementation/application_gui/evaluation_view.py
@@ -1,8 +1,5 @@
-import multiprocessing
-import threading
-from multiprocessing import Queue
-from tkinter import END, HORIZONTAL, VERTICAL, Toplevel, DISABLED, NORMAL
-from tkinter.ttk import Frame, Button, Label, Separator
+from tkinter import HORIZONTAL, VERTICAL
+from tkinter.ttk import Frame, Label, Separator
 
 from application_gui.config_views.resource_path_config_view import ResourcePathFrame
 from application_gui.config_views.segmentation_config_view import SegmentationConfigFrame
@@ -10,13 +7,9 @@ from application_gui.config_views.size_config_view import SizeConfigFrame
 from application_gui.config_views.spm_config_view import SpmConfigFrame
 from application_gui.config_views.texture_config_view import TextureConfigFrame
 from application_gui.process.feedback_view import FeedbackFrame
-from application_gui.process.monitoring import MonitorThread
 from application_gui.process.process_control_view import ProcessControlFrame
-from application_gui.util_views.popups import Popups
-from application_gui.validation_exception import ValidationError
 from evaluator.run_configuration import RunConfiguration
 from evaluator.simulation import Evaluator
-from utils.log import CompositeLogger, ConsoleLogger, QueueLogger
 
 
 class EvaluationFrame(Frame):
@@ -74,76 +67,41 @@ class EvaluationFrame(Frame):
 
         Separator(self, orient=VERTICAL).grid(row=0, column=2, sticky="ns", rowspan=9)
 
-        self.experiment_controls_frame = ProcessControlFrame(self, self.start_experiment, self.stop_experiment)
-        self.experiment_controls_frame.grid(row=0, column=3, rowspan=2)
-
-        self.feedback_frame = FeedbackFrame(self)
-        self.feedback_frame.grid(row=2, column=3, rowspan=7)
+        self.experiment_controls_frame = ProcessControlFrame(self, self.config_extractor, EvaluationFrame.task_starter)
+        self.experiment_controls_frame.grid(row=0, column=3, rowspan=9)
 
         self.grid()
 
-    def stop_experiment(self):
-        self.experiment_controls_frame.set_experiment_running(False)
-        if self.experiment_process is not None:
-            self.experiment_process.terminate()
-            self.experiment_process.join()
-            self.monitor_thread.set_running(False)
-
-    def start_experiment(self):
-        self.feedback_frame.reset()
-        self.experiment_controls_frame.set_experiment_running(True)
-        try:
-            sigma, tau, w_pos = self.segmentation_config_frame.get_values()
-            self.configuration.qs_sigma = sigma
-            self.configuration.qs_tau = tau
-            self.configuration.qs_with_position = w_pos
-
-            spm_model, spm_threshold, spm_type, spm_area = self.spm_config_frame.get_values()
-            self.configuration.spm_model_path = spm_model
-            self.configuration.spm_threshold = spm_threshold
-            self.configuration.spm_type = spm_type
-            self.configuration.spm_neighbour_area = spm_area
-
-            text_model, text_type, text_area = self.texture_config_frame.get_values()
-            self.configuration.texture_model_path = text_model
-            self.configuration.texture_detection_type = text_type
-            self.configuration.texture_detection_area = text_area
-
-            use_rs, height, width = self.size_config_frame.get_values()
-            if use_rs == 1:
-                self.configuration.size = (height, width)
-
-            test_path_in, test_path_exp, test_path_res, test_path_log = self.resource_paths_frame.get_values()
-            self.configuration.test_path_in = test_path_in
-            self.configuration.test_path_expected = test_path_exp
-            self.configuration.results_path = test_path_res
-            self.configuration.test_path_logging = test_path_log
-
-            # start experiment
-            process_queue = Queue()
-            self.experiment_process = RunExperiment(self.configuration, process_queue)
-            self.experiment_process.daemon = True
-            self.experiment_process.start()
-
-            self.monitor_thread = MonitorThread(self.feedback_frame, process_queue)
-            self.monitor_thread.start()
-
-        except ValidationError as e:
-            Popups.show_error_popup(self, str(e), e.errors)
-            self.experiment_controls_frame.set_experiment_running(False)
-
-
-class RunExperiment(multiprocessing.Process):
-    def __init__(self, configuration, process_queue):
-        super(RunExperiment, self).__init__()
-        self.configuration = configuration
-        self.process_queue = process_queue
-
-    def run(self):
-        print("Started thread")
-        logger = CompositeLogger([ConsoleLogger(), QueueLogger(self.process_queue)])
-        evaluator = Evaluator(self.configuration, logger)
+    @staticmethod
+    def task_starter(config, logger):
+        evaluator = Evaluator(config, logger)
         evaluator.run_validation()
 
+    def config_extractor(self):
+        sigma, tau, w_pos = self.segmentation_config_frame.get_values()
+        self.configuration.qs_sigma = sigma
+        self.configuration.qs_tau = tau
+        self.configuration.qs_with_position = w_pos
 
+        spm_model, spm_threshold, spm_type, spm_area = self.spm_config_frame.get_values()
+        self.configuration.spm_model_path = spm_model
+        self.configuration.spm_threshold = spm_threshold
+        self.configuration.spm_type = spm_type
+        self.configuration.spm_neighbour_area = spm_area
 
+        text_model, text_type, text_area = self.texture_config_frame.get_values()
+        self.configuration.texture_model_path = text_model
+        self.configuration.texture_detection_type = text_type
+        self.configuration.texture_detection_area = text_area
+
+        use_rs, height, width = self.size_config_frame.get_values()
+        if use_rs == 1:
+            self.configuration.size = (height, width)
+
+        test_path_in, test_path_exp, test_path_res, test_path_log = self.resource_paths_frame.get_values()
+        self.configuration.test_path_in = test_path_in
+        self.configuration.test_path_expected = test_path_exp
+        self.configuration.results_path = test_path_res
+        self.configuration.test_path_logging = test_path_log
+
+        return self.configuration
--- implementation/application_gui/process/process_control_view.py
+++ implementation/application_gui/process/process_control_view.py
@@ -1,28 +1,57 @@
+from multiprocessing import Queue
+import multiprocessing
 from tkinter import DISABLED, NORMAL
 from tkinter.ttk import Frame, Button
 
+from application_gui.process.feedback_view import FeedbackFrame
+from application_gui.process.monitoring import MonitorThread
+from application_gui.util_views.popups import Popups
+from application_gui.validation_exception import ValidationError
+from utils.log import CompositeLogger, ConsoleLogger, QueueLogger
+
 
 class ProcessControlFrame(Frame):
-    def __init__(self, parent, start_experiment, stop_experiment):
+    """
+    Controls a task (detection, evaluation, etc)
+
+    has a frame for feedback, buttons to start and stop the task
+    creates a separate process for the task and a monitor thread
+    binds them with a Queue
+
+    monitor thread displays messages in feedback frame
+    """
+
+    def __init__(self, parent, config_extractor, task_starter):
         Frame.__init__(self, parent)
+
+        self.feedback_frame = None
 
         # experiment controls
         self.start_experiment_button = None
         self.stop_experiment_button = None
         self.experiment_running = False
 
-        # actions
-        self.start_experiment = start_experiment
-        self.stop_experiment = stop_experiment
+        # extracts a configuration object
+        self.config_extractor = config_extractor
+        # takes a config and a logger and starts a task
+        self.task_start = task_starter
+
+        # process
+        self.experiment_process = None
+        # monitor thread
+        self.monitor_thread = None
 
         self.init_view()
 
     def init_view(self):
-        self.start_experiment_button = Button(self, text="Start experiment", command=self.start_experiment)
-        self.start_experiment_button.grid(row=0, column=3, rowspan=1)
+        self.start_experiment_button = Button(self, text="Start experiment", command=self.start_process)
+        self.start_experiment_button.grid(row=0, column=0)
 
-        self.stop_experiment_button = Button(self, text="Stop experiment", command=self.stop_experiment, state=DISABLED)
-        self.stop_experiment_button.grid(row=1, column=3, rowspan=1)
+        self.stop_experiment_button = Button(self, text="Stop experiment", command=self.stop_process, state=DISABLED)
+        self.stop_experiment_button.grid(row=1, column=0)
+
+        self.feedback_frame = FeedbackFrame(self)
+        self.feedback_frame.grid(row=2, column=0)
 
     def set_experiment_running(self, value):
         self.experiment_running = value
@@ -32,3 +61,42 @@ class ProcessControlFrame(Frame):
         else:
             self.start_experiment_button.config(state=NORMAL)
             self.stop_experiment_button.config(state=DISABLED)
+
+    def start_process(self):
+        self.feedback_frame.reset()
+        self.set_experiment_running(True)
+        try:
+            configuration = self.config_extractor()
+
+            # start experiment
+            process_queue = Queue()
+            self.experiment_process = RunExperiment(configuration, process_queue, self.task_start)
+            self.experiment_process.daemon = True
+            self.experiment_process.start()
+
+            self.monitor_thread = MonitorThread(self.feedback_frame, process_queue)
+            self.monitor_thread.start()
+
+        except ValidationError as e:
+            Popups.show_error_popup(self, str(e), e.errors)
+            self.set_experiment_running(False)
+
+    def stop_process(self):
+        self.set_experiment_running(False)
+        if self.experiment_process is not None:
+            self.experiment_process.terminate()
+            self.experiment_process.join()
+            self.monitor_thread.set_running(False)
+
+
+class RunExperiment(multiprocessing.Process):
+    def __init__(self, configuration, process_queue, task_starter):
+        super(RunExperiment, self).__init__()
+        self.configuration = configuration
+        self.process_queue = process_queue
+        self.task_starter = task_starter
+
+    def run(self):
+        print("Started thread")
+        logger = CompositeLogger([ConsoleLogger(), QueueLogger(self.process_queue)])
+        self.task_starter(self.configuration, logger)
